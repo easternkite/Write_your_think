e52ac028e77c5e3964d948d187c9e69552996aaf
blucky8649 <blucky4511@gmail.com>
2022-02-09 22:26:32 +0900

    Diary 데이터 클래스 간소화

--- app/src/main/java/com/multimedia/writeyourthink/Diary.kt
+++ app/src/main/java/com/multimedia/writeyourthink/Diary.kt
@@ -1,32 +1,10 @@
 package com.multimedia.writeyourthink
 
-class Diary {
-    var userUID: String? = null
-        private set
-    var profile: String? = null
-    var where: String? = null
-    var contents: String? = null
-    var date: String? = null
-    var location: String? = null
-
-    constructor() {}
-    constructor(
-        userUID: String?,
-        profile: String?,
-        where: String?,
-        contents: String?,
-        date: String?,
-        location: String?
-    ) {
-        this.userUID = userUID
-        this.profile = profile
-        this.where = where
-        this.contents = contents
-        this.date = date
-        this.location = location
-    }
-
-    fun setUserName(userUID: String?) {
-        this.userUID = userUID
-    }
-}
+data class Diary (
+    var userUID: String? = null,
+    var profile: String? = null,
+    var where: String? = null,
+    var contents: String? = null,
+    var date: String? = null,
+    var location: String? = null,
+)
